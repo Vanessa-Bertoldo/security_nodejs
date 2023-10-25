55a9cdd3e16e764d98a9e362369c82edcd6dc281
vanessa <vanessa.bert311@gmail.com>
2023-10-25 13:58:26 -0300

    create token access

---
+++ api/config/jsonSecret.js
@@ -0,0 +1,3 @@
+module.exports = {
+    secret: "b81bd535399c190319a11df0a68f2169"
+}
--- api/services/authService.js
+++ api/services/authService.js
@@ -1,17 +1,35 @@
 const database = require('../models')
+const { compare } = require('bcryptjs')
+const { sign } = require('jsonwebtoken')
+const jsonSecret = require('../config/jsonSecret')
 
 class AuthService{
     async login(dto){
-        const usuario = await database.usuario.findOnde({
+        const user = await database.usuarios.findOne({
             attributes: ['id', 'email', 'senha'],
             where: {
                 email: dto.email
             }
         })
 
-        if(!usuario){
+        if(!user){
             throw new Error('Usuario não cadastrado')
         }
+
+        const equalPassw = await compare(dto.senha, user.senha)
+
+        if(!equalPassw){
+            throw new Error('Usuario ou senha inválido')
+        }
+
+        const acessToken = sign({
+            id: user.id,
+            email: user.email
+        }, jsonSecret.secret, {
+            expiresIn: 86400
+        })
+
+        return {acessToken}
     }
 }
 module.exports = AuthService
